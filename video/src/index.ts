f57364f688d4953e6a5395bddcf28baeaa079bee
Vincent Vella <vincevella0@gmail.com>
2021-09-28 02:38:45 -0400

    feat: add skaffold, update config maps, update tilt config, fix persistent storage

--- video/src/index.ts
+++ video/src/index.ts
@@ -1,6 +1,5 @@
 import 'dotenv-safe/config'
 import express, { Application } from 'express';
-import ngrok from 'ngrok';
 import { routes } from './routes';
 
 const app: Application = express();
@@ -9,7 +8,3 @@ app.use(routes)
 app.listen(process.env.PORT, () =>
   console.log(`Server listening on port ${process.env.PORT}!`),
 );
-
-ngrok.connect(parseInt(process.env.PORT, 0)).then((url) => {
-  console.log(`Server forwarded to public url ${url}`);
-});
