e4d0e0223a09473b5da4058529272265bbca7acd
Vincent Vella <vincevella0@gmail.com>
2021-10-19 22:23:12 -0400

    fix: account -> user

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AccountModule } from './user/user.module';
+import { UserModule } from './user/user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [PrismaModule, AccountModule, AuthenticationModule, ProfileModule],
+  imports: [PrismaModule, UserModule, AuthenticationModule, ProfileModule],
 })
 export class AppModule { }
