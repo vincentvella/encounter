8e97d814a52f3f33e92b629d8cff52dd808a3318
Vincent Vella <vincevella0@gmail.com>
2022-01-13 00:39:05 -0500

    fix: debug port

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
-import * as cors from 'cors'
+console.log('starting', process.env)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
